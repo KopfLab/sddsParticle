d02726618f598a5e2b1bfe407f87fe3087c32182
Sebastian Kopf <seb.kopf@gmail.com>
2026-04-18 10:14:26 -0600

    make selector table loading less verbose

--- R/app_module_selector_table.R
+++ R/app_module_selector_table.R
@@ -155,8 +155,7 @@ module_selector_table_server <- function(
       # info
       log_info(
         ns = ns,
-        "(re-) rendering selection table",
-        user_msg = "Loading table"
+        "(re-) rendering selection table" #,user_msg = "Loading table"
       )
       # get the table
       table <-
@@ -676,8 +675,7 @@ module_selector_table_server <- function(
     log_success(
       ns = ns,
       "is_table_reloaded() now returns ",
-      n,
-      user_msg = "Complete"
+      n #,user_msg = "Complete"
     )
   })
 
